ef00d9435b86731ffc6e8f11f7ce12e6ecd32767
Alicia <alicia.tarifa.8@gmail.com>
2022-03-28 17:03:40 -0300

    Ejercicio 1

---
+++ src/cls_ejercicio_1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author alicia
+ *
+ */
+/*
+ * Este programa suma 5 y 4 dando el resultado en la consola
+ * utilizando el método System.out.println ()
+ */
+public class cls_ejercicio_1 {
+	
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 4;
+		int result = num1 + num2;
+		
+		System.out.println("La suma de " + num1 + " y " + num2 + " es: " + result);
+		
+		
+}}
