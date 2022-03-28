5b460024ecd8a2f007b7621f35208e8c27ea4bef
Alicia <alicia.tarifa.8@gmail.com>
2022-03-28 18:59:54 -0300

    Ejercicio 3

---
+++ src/cls_ejercicio_3.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author alicia
+ *
+ */
+public class cls_ejercicio_3 {
+
+	String nombre = "Alicia";
+	String apellido = "Tarifa";
+	int edad = 35; 
+	public static void main(String[] args) {
+	
+	// String 1nombre = "Alicia"; 
+	/* --> Es un identificador inválido porque comienza con el número 1
+	*/
+	
+	// String @pellido = "Tarifa"; 
+	/* --> Es un identificador inválido porque comienza con arroba
+	*/
+	// int (edad = 35; /* --> Es un identificador inválido porque comienza con paréntesis
+
+
+        
+	System.out.println("Alicia");
+	System.out.println("Tarifa");
+	System.out.println("35");
+}}
