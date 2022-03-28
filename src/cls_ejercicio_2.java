c2da6cda020ba4ef1d50b24a5e169dad318a674a
Alicia <alicia.tarifa.8@gmail.com>
2022-03-28 17:54:48 -0300

    Ejercicio 2

---
+++ src/cls_ejercicio_2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * Programa HoraDeComer
+ * @author alicia
+ * @version 1.0
+ * @see Refencias
+ */
+
+/* El programa HoraDeComer se utiliza para aplicar System.out.println()
+ * mostrando el mensaje en consola
+ */
+
+// El método System.out.println() marca una línea nueva
+
+public class cls_ejercicio_2 {
+	
+	public void consola() {
+				
+		System.out.println("Hora");
+		System.out.println("de");
+		System.out.println("Comer");
+	}
+}
